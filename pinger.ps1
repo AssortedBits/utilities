829e3d6e3a2ae9142cbb1751ef3d4b7e6d3c6cf0
BargainBasementBits <keithatschool@yahoo.com>
2022-04-12 23:37:35 -0400

    pinger: compactified the output

--- pinger.ps1
+++ pinger.ps1
@@ -3,21 +3,30 @@ Param
     [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
     [System.Uri[]]$addresses
 )
+
+$pingTimeout = 5
+
 while($true) {
+    Get-Date -Format "HH:mm:ss"
+    echo ""
     $jobs = @()
     #Make sure we don't hammer faster than once every 3s
-    $waitJerb = Start-Job -ScriptBlock { Start-Sleep -Seconds 4 }
+    $waitJerb = Start-Job -ScriptBlock { Start-Sleep -Seconds ($using:pingTimeout) }
     foreach ($addr in $addresses) {
         $jobs += Start-Job -ScriptBlock {
-            $result = Test-Connection -TargetName $using:addr -Count 1
-            #because Test-Connection returns null when packet is dropped.
-            if($null -eq $result) {
-                echo "$using:addr : packet dropped"
-            } else {
-                echo ($result | Format-Table -Property Address,Latency)
+            $result = Test-Connection -TargetName $using:addr -Count 1 -TimeoutSeconds $using:pingTimeout
+            $obj = New-Object -TypeName PsObject -Property @{
+                Address = $using:addr
+                Latency = if ($null -eq $result) {"dropped"} else {$result.Latency}
             }
+            return $obj
         }
     }
-    foreach ($job in $jobs) { Receive-Job $job -Wait}
+    $results = @()
+    foreach ($job in $jobs) {
+        $results += Receive-Job $job -Wait
+    }
+    $results | Select-Object -Property Address,Latency
+    echo ""
     Receive-Job $waitJerb -Wait
 }
